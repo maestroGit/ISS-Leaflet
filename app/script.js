996e39962b7e89d0b0751da6d9b0a042730aa344
walkexperience <walkexpereince@gmail.com>
2020-11-04 14:03:13 +0100

    Menu setView

--- app/script.js
+++ app/script.js
@@ -1,75 +1,126 @@
-// Creamos mapa y el origen de la cap
-const map = L.map('map-template').setView([20, 50], 2);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// Creamos mapa y el origen de la capa con tiles
+const map = L.map("map-template").setView([20, 50], 2);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-
-
+// Marcadores
 // Creamos marcador manual con coprdenadas fijas
-const marker = L.marker([51.5, -0.09]);
-marker.bindPopup('Hello There');
-map.addLayer(marker);
+const markerIcon = L.marker([51.5, -0.09]);
+const latitude2 = 0;
+const longitude2 = 0;
+markerIcon.setLatLng([latitude2, longitude2]);
 
-// Diseño de marcador con icon leaflet obj
+// Diseño de iconos leaflet obj
 const myIcon = L.icon({
-    iconUrl: 'https://walkexperience.org/wp-content/uploads/2020/06/Walk-logo-97.png',
-    iconSize: [24, 25], // size of the icon
-    iconAnchor: [12, 25]
+  iconUrl:
+    "https://walkexperience.org/wp-content/uploads/2020/06/Walk-logo-97.png",
+  iconSize: [24, 25], // size of the icon
+  iconAnchor: [12, 25],
+});
 
-});
 const issIcon = L.icon({
-    iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/International-Space-Station_mark.svg/268px-International-Space-Station_mark.svg.png',
-    iconSize: [45, 45],
-    iconAnchor: [12, 45]
+  iconUrl:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/International-Space-Station_mark.svg/268px-International-Space-Station_mark.svg.png",
+  iconSize: [25, 25],
+  iconAnchor: [12, 45],
 });
-L.marker([41.5, 1.5], { icon: myIcon }).addTo(map); //ecuador 0 0
+
+const issIconBig = L.icon({
+  iconUrl:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/International-Space-Station_mark.svg/268px-International-Space-Station_mark.svg.png",
+  iconSize: [85, 85],
+  iconAnchor: [12, 25],
+});
+
+// Añadimos marcadores
+map.addLayer(markerIcon);
+markerIcon.bindPopup(
+  '<a href="https://walkexperience.org/">WalkExperience</a>'
+);
+
+const myIconMarker = L.marker([41.5, 1.5], { icon: myIcon }).addTo(map); //ecuador 0 0
+myIconMarker.bindPopup(
+  '<a href="https://walkexperience.org/">WalkExperience</a>'
+);
 
 //Incorporar posición del usuario
 //Con el protocolo webscket el servidor http envía eventos en tiempo real cuando los usuarios se conectan
 
-const url_apiISS = 'http://api.open-notify.org/iss-now.json';
 // Petición API ISS
+const url_apiISS = "http://api.open-notify.org/iss-now.json";
+
 async function getISS() {
-    const res = await fetch(url_apiISS);
-    const data = await res.json();
-    const timeSeconds = data.timestamp;
-    const { latitude, longitude } = data.iss_position;
+  const res = await fetch(url_apiISS);
+  const data = await res.json();
+  const timeSeconds = data.timestamp;
+  const { latitude, longitude } = data.iss_position;
 
-    // Transformar string substring() devuelve la parte de string entre los índices inicial y final, o hasta el final de la cadena. Así se muestran menos decimales en lat y lng
-    const latitudeDos = latitude.charAt(0)=='-' ? latitude.substring(0,6): latitude.substring(0,5);
-        console.log(latitudeDos);
-    const longitudeDos = longitude.charAt(0)=='-' ? longitude.substring(0,6) : longitude.substring(0,5);
-        console.log(longitudeDos);
-    document.getElementById('lat').textContent = latitudeDos;
-    document.getElementById('lng').textContent = longitudeDos;
+  // Transformar string substring() devuelve la parte de string entre los índices inicial y final, o hasta el final de la cadena. Así se muestran menos decimales en lat y lng
+  const latitudeDos =
+    latitude.charAt(0) == "-"
+      ? latitude.substring(0, 6)
+      : latitude.substring(0, 5);
+  console.log(latitudeDos);
+  const longitudeDos =
+    longitude.charAt(0) == "-"
+      ? longitude.substring(0, 6)
+      : longitude.substring(0, 5);
+  console.log(longitudeDos);
+  document.getElementById("lat").textContent = latitudeDos;
+  document.getElementById("lng").textContent = longitudeDos;
 
-    // Creamos marcador ISS
-    const markerISS = L.marker([latitude, longitude], { icon: issIcon });
-    
-    // time
-    // Create a new JavaScript Date object based on the timestamp
-    // multiplied by 1000 so that the argument is in milliseconds, not seconds.
-    let date = new Date(timeSeconds * 1000);
-    // Hours part from the timestamp
-    let hours = date.getHours();
-    // Minutes part from the timestamp
-    let minutes = "0" + date.getMinutes();
-    // Seconds part from the timestamp
-    let seconds = "0" + date.getSeconds();
-    // Will display time in 10:30:23 format
-    let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+  // Creamos marcador ISS
+  const markerISS = L.marker([latitude, longitude], { icon: issIcon });
 
-    // Ver hora de paso en pop up
-    markerISS.bindPopup('ISS passed at ' + formattedTime);
-    map.addLayer(markerISS);
-    setTimeout(getISS, 20000);
-    
+  // Captamos evento zoomend - in order to change the size of the markers
+  // Resize marker icons depending on zoom level issue #1 -I did not resolve it
+  // I Create another biggest marker that will be changed on the map zooming
+
+  map.on("zoomend", function () {
+    var currentZoom = map.getZoom();
+    console.log(currentZoom);
+    if (map.getZoom() > 4) {
+      markerISS.setIcon(issIconBig);
+      //alert('works')
+    } else {
+      markerISS.setIcon(issIcon);
+    }
+  });
+
+  // Center view map with center ISS -setView()
+  const zoomISS = document.getElementById("view-zoom");
+  zoomISS.addEventListener("click", function () {
+    map.setView([latitude, longitude], 8);
+  });
+  //map.setView([latitude, longitude],5)
+  // time
+  // Create a new JavaScript Date object based on the timestamp
+  // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+  let date = new Date(timeSeconds * 1000);
+  // Hours part from the timestamp
+  let hours = date.getHours();
+  // Minutes part from the timestamp
+  let minutes = "0" + date.getMinutes();
+  // Seconds part from the timestamp
+  let seconds = "0" + date.getSeconds();
+  // Will display time in 10:30:23 format
+  let formattedTime =
+    hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
+
+  // Ver hora de paso en pop up
+  markerISS.bindPopup("ISS passed at " + formattedTime);
+  map.addLayer(markerISS);
+  // refreshing new data from the ISS API
+  setTimeout(getISS, 2000);
 }
 
 getISS().catch((err) => {
-    console.log('In catch !!!')
-    console.log(err);
+  console.log("In catch !!!");
+  console.log(err);
 });
 
-setTimeout(getISS(), 2000);
+// "loop" with setTimeout() after the fetching is done
+// If the request takes more than 1 second (maybe a slow server or internet connection issues) over a long period of time, setInterval() will push lots of callback calls to the event queue.
+setTimeout(getISS, 2000);
